0436516e9543adef38f9b4c1073d885d587214f9
davidmoloney1994 <davidmoloney1994@gmail.com>
2017-06-08 11:49:07 +0100

    Add files via upload

---
+++ MCMCstan.R
@@ -0,0 +1,79 @@
+setwd('/Users/David/Dropbox/Oxford/Dissertation')
+getwd()
+
+
+library("rstan")
+rstan_options(auto_write = TRUE)
+options(mc.cores = parallel::detectCores())
+library(deSolve)
+
+
+data = read.csv("datatest.csv", header=T, stringsAsFactors=F)
+
+data[,3] = as.numeric(data[,3])
+dat = na.omit(data)
+
+dat[,3] = dat[,3]/100
+
+head(dat)
+relapse = NULL
+
+for(i in 1:(length(dat[,1]) - 1))
+{
+  if(dat[i+1,3] > dat[i,3] + 0.05 && dat[i,1] == dat[i+1,1])
+    relapse = c(relapse, dat[i,1])
+}
+
+relapse = unique(relapse)
+remission = setdiff(1:69,relapse)
+
+remissionDat = dat[which(dat[,1] %in% remission),]
+relapseDat = dat[which(dat[,1] %in% relapse),]
+
+remissionDat = aggregate(BCR.ABL1.ABL1.... ~ Month, data = remissionDat, mean)
+
+plot(remissionDat$Month, remissionDat$BCR.ABL1.ABL1....)
+
+
+luk_dat <- list(T = length(remissionDat[,1]), 
+                R = remissionDat$BCR.ABL1.ABL1....,
+                t0 = -1,
+                ts = remissionDat$Month)
+
+rstan_options(auto_write = TRUE)
+fit <- stan(file = 'odemcmc.stan', data = luk_dat, 
+            iter = 1000, chains = 1)
+print(fit)
+
+la = extract(fit, permuted = TRUE) # return a list of arrays 
+thetamcmc = la$theta
+y0mcmc = la$y0
+
+theta = apply(thetamcmc, 2, mean)
+y0 = apply(y0mcmc, 2, mean)
+
+parameters = c(ax = theta[1], bx = theta[2], cx = theta[3],
+               dx = theta[4], ex = theta[5], ay = theta[6],
+               by = theta[7], ey = theta[8])
+
+state = c(x0 = y0[1], x1 = y0[2], x2 = y0[3], y0 = y0[4], y1 = y0[5])
+
+
+desystem <- function(t, state, parameters) {
+  with(as.list(c(state, parameters)), {
+    dx0 <- ax * x0 * (1 - x0 - x1 - x2 - y0 - y1) - bx * x0
+    dx1 <- bx * x0 + cx * x1 * (1 - x0 - x1 - x2 - y0 - y1) - dx * x1
+    dx2 <- dx * x1 - ex * x2
+    dy0 <- ay * y0 * (1 - x0 - x1 - x2 - y0 - y1) - by * y0
+    dy1 <- by * y0 - ey * y1
+    list(c(dx0, dx1, dx2, dy0, dy1))
+  })
+}
+
+times = seq(0, 70, by = 0.1)
+out = ode(y = state, times = times, func = desystem, parms = parameters)
+head(out)
+
+R = out[,6]/(out[,6] + 2*out[,4])
+lines(out[,1], R, col='red')
+
---
+++ odemcmc.stan
@@ -0,0 +1,18 @@
+functions {
+	real[] m1ode(real t, real[] y, real[] theta, real[] x_r, int[] x_i) {        	real dydt[5];        	dydt[1] = theta[1] * y[1] * (1-y[1]-y[2]-y[3]-y[4]-y[5]) - theta[2] * y[1];        	dydt[2] = theta[2] * y[1] + theta[3] * y[2] * (1-y[1]-y[2]-y[3]-y[4]-y[5]) - theta[4] * y[2];
+		dydt[3] = theta[4] * y[2] - theta[5] * y[3];
+		dydt[4] = theta[6] * y[4] * (1-y[1]-y[2]-y[3]-y[4]-y[5]) - theta[7] * y[4];
+		dydt[5] = theta[7] * y[4] - theta[8] * y[5];        	return dydt;
+}      
+}data {	int<lower=1> T;      	real R[T];      	real t0;      	real ts[T];}transformed data {      	real x_r[0];      	int x_i[0];
+}parameters {      	real<lower=0> sigma;
+      	real<lower=0,upper=0.2> y0[5];      	real theta[8];}
+model {      	real y_hat[T,5];
+      	real R_hat[T];      	sigma ~ uniform(0,1);
+      	theta ~ normal(0.5,1);
+	y0 ~ uniform(0,0.2);      	y_hat = integrate_ode_rk45(m1ode, y0, t0, ts, theta, x_r, x_i);
+	
+      	for (t in 1:T) {
+		R_hat[t] = y_hat[t,5]/(y_hat[t,5] + 2 * y_hat[t,3]);        	//R[t] ~ lognormal(log(R_hat[t]) - (sigma^2)/2 , sigma);
+		R[t] ~ normal(R_hat[t], sigma);
+      	}}
